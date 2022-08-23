fdc1de1aa9d0c8f5f4ad6884b4eed9da3877ea11
raheel <raheelmushtaq41@gmail.com>
2022-08-24 01:41:02 +0500

    firease crashlytics and firebase app added.

--- ios/BFF/AppDelegate.mm
+++ ios/BFF/AppDelegate.mm
@@ -17,7 +17,7 @@
 
 #import <react/config/ReactNativeConfig.h>
 
-//#import <Firebase.h>
+#import <Firebase.h>
 //#import <FirebaseCrahlytics.h>
 
 @interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
@@ -59,7 +59,7 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   [RNSplashScreen show];
-//  [FIRApp configure];
+  [FIRApp configure];
 
   return YES;
 }
