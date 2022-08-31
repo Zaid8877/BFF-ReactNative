e9bd394d7c80c29cd245cfcc891739be2b36b6b4
raheel <raheelmushtaq41@gmail.com>
2022-09-01 03:11:55 +0500

    hours removed.

--- src/Components/CallDurationComponent/index.js
+++ src/Components/CallDurationComponent/index.js
@@ -32,7 +32,7 @@ const CallDurationComponent = ({joinSucceded, callEnded}) => {
         var hDisplay = h > 0 ? h + " : " : "00 : ";
         var mDisplay = m > 0 ? m + " : " : "00 : ";
         var sDisplay = s > 0 ? s + "" : "00";
-        return hDisplay + mDisplay + sDisplay;
+        return /*hDisplay + */mDisplay + sDisplay;
     }
     const renderComponentData=()=>{
         if(joinSucceded || callEnded){
